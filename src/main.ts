c7dde463176d3c5af4b07a6f07b4e624cdbc316a
Egor <readg.kill@mail.ru>
2023-10-20 17:59:29 +0300

    add cors

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-import { PrismaService } from './prisma.service'
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule)
-
-  app.setGlobalPrefix('api')
-  app.enableCors()
-	await app.listen(4200) 
+	const app = await NestFactory.create(AppModule, { cors: true })
+	app.setGlobalPrefix('api')
+	app.enableCors()
+	await app.listen(4200)
 }
 bootstrap()
