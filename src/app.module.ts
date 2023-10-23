e6865ff29b0c95f5c72baac6faf25c38de44374a
Egor <readg.kill@mail.ru>
2023-10-23 17:44:12 +0300

    test deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,19 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { ConfigModule } from '@nestjs/config'
+import { ServeStaticModule } from '@nestjs/serve-static'
+import { path } from 'app-root-path'
+import { AuthModule } from './auth/auth.module'
+import { CategoryModule } from './category/category.module'
+import { OrderModule } from './order/order.module'
+import { PaginationModule } from './pagination/pagination.module'
+import { PrismaModule } from './prisma/prisma.module'
+import { ProductModule } from './product/product.module'
+import { ReviewModule } from './review/review.module'
+import { StatisticsModule } from './statistics/statistics.module'
+import { UploadModule } from './upload/upload.module'
+import { UserModule } from './user/user.module'
 
 @Module({
 	imports: [
@@ -20,21 +33,21 @@ import { AppService } from './app.service'
 		// }),
 		//
 		// TESTING deploy
-		// ServeStaticModule.forRoot({
-		// 	rootPath: `${path}/uploads`,
-		// 	serveRoot: '/uploads'
-		// }),
-		// ConfigModule.forRoot(),
-		// AuthModule,
-		// UserModule,
-		// ProductModule,
-		// ReviewModule,
-		// CategoryModule,
-		// OrderModule,
-		// StatisticsModule,
-		// PaginationModule,
-		// UploadModule,
-		// PrismaModule
+		ServeStaticModule.forRoot({
+			rootPath: `${path}/uploads`,
+			serveRoot: '/uploads'
+		}),
+		ConfigModule.forRoot(),
+		AuthModule,
+		UserModule,
+		ProductModule,
+		ReviewModule,
+		CategoryModule,
+		OrderModule,
+		StatisticsModule,
+		PaginationModule,
+		UploadModule,
+		PrismaModule
 	],
 	controllers: [AppController],
 	providers: [AppService]
